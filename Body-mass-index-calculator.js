10693746e6b89189dd1ef43ac173ca67642c2081
kafayat.a.adeoye <51514786+kaffy90@users.noreply.github.com>
2020-04-15 12:10:56 +0100

    Update Body-mass-index-calculator.js

--- Body-mass-index-calculator.js
+++ Body-mass-index-calculator.js
@@ -1,6 +1,6 @@
 function bmiCalculator (weight, height) {
  
- var bmi = Math.round(weight/Math.pow(height, 2));
+ var bmi = Math.round(weight/Math.pow(height, 2)); // calculate the body mass index with this formula and round up the value
  
  if (bmi < 18.5) {
       return "Your BMI is " + bmi + "," + " so you are underweight.";
